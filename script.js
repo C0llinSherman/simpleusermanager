35d8b0217f65f073828412a8ac4afaaad94fdf9e
Collin Sherman <collin@Collins-MacBook-Air.local>
2022-02-03 12:46:33 -0700

    working version

--- script.js
+++ script.js
@@ -2,88 +2,65 @@ const express = require('express');
 const path = require('path')
 const app = express()
 const fs = require('fs')
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-const csv = require('csv-parser')
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug')
+
+
 app.get('/', (req, res) => {
-    res.render('index', {myTitle: 'This is a title', myHeading: 'This is a heading'})
+    res.render('index')
 })
 app.get('/createUser', (req, res) => {
-    res.render('create')
+    let users = fs.readFileSync('./users.json')
+    users = JSON.parse(users)
+    let userIDCount = +(users["userCount"]) + 1
+    res.render('create', {countKey: userIDCount})
 })
 app.post('/createUser', (req, res) => {
-    let userID = req.body.userID;
-    let name= req.body.name;
-    let email = req.body.email;
-    let age = req.body.age;
-    console.log(req.body)
-    // res.send(`Got it${userID} ${name}, ${email}, ${age}`)
+    let users = fs.readFileSync('./users.json')
+    users = JSON.parse(users)
+    let countKey = users['userCount']
+    const newUser = req.body
+    users[req.body.userID] = newUser
+    users["userCount"] = countKey + 1
+    fs.writeFileSync('./users.json', JSON.stringify(users, null, 2))
     res.redirect('/userListing')
-    let count = 1
-    fs.createReadStream('data.csv')
-    .pipe(csv())
-    .on('data', (row) => {
-        count += 1
-    })
-    .on('end', () => {
-        console.log('done parsing the file')
-        csvWriter()
-    })
-    function csvWriter() {
-        const csvWriter = createCsvWriter({
-            path: 'data.csv',
-            header: [
-                { id: 'entrynumber', title: 'entrynumber' },
-                { id: 'date', title: 'date' },
-                { id: 'userID', title: 'userID' },
-                { id: 'name', title: 'name' },
-                { id: 'email', title: 'email' },
-                { id: 'age', title: 'age' },
-            ],
-            append: true,
-        });
-        let today = new Date()
-        const entry = [
-            {
-                entrynumber: count,
-                date: today,
-                userID: userID,
-                name: name,
-                email: email,
-                age: age
-            }];
-        csvWriter.writeRecords(entry)
-            .then(() => console.log('..DONE'));
-    }
 })
 
 app.get('/userListing', (req, res) => {
-    let userArray = [{"name": "John"},{"name": "Mary"}]
-    fs.createReadStream('data.csv')
-    .pipe(csv())
-    .on('data', (row) => {
-        console.log(JSON.stringify(row))
-        let temp = []
-        // temp = JSON.stringify(row).substring(1).slice(0,-1)
-        // console.log(temp.slice(1, -1))
-        userArray.push(temp)
-    })
-    .on('end', () => {
-        console.log('done parsing the file')
-        console.log(userArray)
-    })
-    // console.log(userArray)
-    res.render('listings', {userArray: userArray})
+    let users = fs.readFileSync('./users.json')
+    users = JSON.parse(users)
+    delete users['userCount']
+    res.render('listings', {userArray: users})
 })
 
-app.get('/editUser', (req, res) => {
-    res.render('edit')
+app.get('/editUser/:id', (req, res) => {
+    let users = fs.readFileSync('./users.json')
+    users = JSON.parse(users)
+    let userID = req.params.id;
+    if(!users[userID]){
+        res.send("User does not Exist")
+    }
+    let currentUser = users[userID]
+    res.render('edit', {currentUser: currentUser})
 })
 
-app.listen(3001, () => {
+app.post('/deleteUser/:id', (req, res) => {
+    let users = fs.readFileSync('./users.json')
+    users = JSON.parse(users)
+    let userID = req.params.id;
+    delete users[userID]
+    fs.writeFileSync('./users.json', JSON.stringify(users, null, 2))
+    console.log(users)
+    res.redirect('/userListing')
+})
+
+app.get('/*', (req,res) => {
+    res.send("This page does not exist")
+})
+
+app.listen(3000, () => {
     console.log('listening on port 3000')
 })
